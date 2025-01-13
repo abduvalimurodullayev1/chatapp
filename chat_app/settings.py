9ba80dc6be76454a3e474f48273b75663e414cbb
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2025-01-13 11:57:12 +0500

    Update settings.py

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import environ
 env = environ.Env()
 environ.Env.read_env()
-# SECURITY WARNING: 
+# SECURITY 
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
